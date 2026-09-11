c2035e9d59dcea845bd6a72505a2d6794259301a
PrivatumRH <dbantifun@gmail.com>
2026-09-11 10:01:56 +0100

    feat: add public github repository links to landing page and docs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -432,6 +432,24 @@ function LandingPage() {
                           </svg>
                         </div>
                       </a>
+                      <a
+                        aria-label="PRIVATUM on GitHub"
+                        className="w-inline-block"
+                        href="https://github.com/PrivatumRH/privatum"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social">
+                          <svg fill="none" viewBox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.53 1.032 1.53 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </a>
                     </div>
                   </div>
                 </nav>
@@ -1032,7 +1050,14 @@ function LandingPage() {
 
               <div className="pv-downloads-footer">
                 <div>
-                  <span>Automated releases built and signed for Windows, macOS, and Linux.</span>
+                  <span>Automated releases built and signed for Windows, macOS, and Linux. </span>
+                  <a
+                    href="https://github.com/PrivatumRH/privatum"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View repository on GitHub &rarr;
+                  </a>
                 </div>
                 <div>
                   <span>Robinhood Chain Testnet ID: <code>4663</code></span>
@@ -2714,6 +2739,14 @@ function LandingPage() {
                         >
                           <div>X</div>
                         </a>
+                        <a
+                          className="inner-link w-inline-block"
+                          href="https://github.com/PrivatumRH/privatum"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <div>GitHub</div>
+                        </a>
                       </div>
                     </div>
                   </div>
@@ -2739,6 +2772,24 @@ function LandingPage() {
                           <svg fill="none" viewBox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
                             <path
                               d="M18.9 2H22L15.2 9.8L23.3 22H17L12.1 15.6L6.5 22H3.3L10.6 13.7L2.8 2H9.2L13.6 7.9L18.9 2ZM17.8 20.1H19.5L7.1 3.8H5.3L17.8 20.1Z"
+                              fill="currentColor"
+                            />
+                          </svg>
+                        </div>
+                      </a>
+                      <a
+                        aria-label="PRIVATUM on GitHub"
+                        className="w-inline-block"
+                        href="https://github.com/PrivatumRH/privatum"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <div className="social">
+                          <svg fill="none" viewBox="0 0 24 24" width="100%" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.53 1.032 1.53 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                               fill="currentColor"
                             />
                           </svg>
